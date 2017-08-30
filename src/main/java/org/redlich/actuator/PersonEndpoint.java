c6bd41d5e0bdfc458674f5c36aa949623d7eab3d
Stephane Nicoll <snicoll@pivotal.io>
2017-08-30 15:09:19 +0200

    Formatting

--- src/main/java/org/redlich/actuator/PersonEndpoint.java
+++ src/main/java/org/redlich/actuator/PersonEndpoint.java
@@ -19,39 +19,39 @@ public class PersonEndpoint {
     private final Map<String, Person> people = new HashMap<>();
 
     PersonEndpoint() {
-        this.people.put("mike",new Person("Michael Redlich"));
-        this.people.put("rowena",new Person("Rowena Redlich"));
-        this.people.put("barry",new Person("Barry Burd"));
-        }
+        this.people.put("mike", new Person("Michael Redlich"));
+        this.people.put("rowena", new Person("Rowena Redlich"));
+        this.people.put("barry", new Person("Barry Burd"));
+    }
 
     @ReadOperation
     public List<Person> getAll() {
         return new ArrayList<>(this.people.values());
-        }
+    }
 
     @ReadOperation
     public Person getPerson(@Selector String person) {
         return this.people.get(person);
-        }
+    }
 
     @WriteOperation
-    public void updatePerson(@Selector String name,String person) {
-        this.people.put(name,new Person(person));
-        }
+    public void updatePerson(@Selector String name, String person) {
+        this.people.put(name, new Person(person));
+    }
 
     public static class Person {
         private String name;
 
         Person(String name) {
             this.name = name;
-            }
+        }
 
         public String getName() {
             return this.name;
-            }
+        }
 
         public void setName(String name) {
             this.name = name;
-            }
         }
     }
+}
